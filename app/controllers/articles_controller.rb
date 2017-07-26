87488770c502bb769ab1b577fc2edbd409a0bf38
sdogruyol <dogruyolserdar@gmail.com>
2017-07-26 18:50:24 +0300

    First commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,28 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+
+  def new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+end

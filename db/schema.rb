bf79e59a209e3c052e1ca2f1c023c9581c7740a6
Recep <rahmetsaritekin@gmail.com>
2017-07-26 21:24:07 +0300

    first app is complicated

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170726132854) do
+ActiveRecord::Schema.define(version: 20170726165435) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
 end
